153c869d45c9bd5e98a3442c76b2e7fcec98e1f9
WILLIAM GEORGE <williameverettgeorge@icloud.com>
2025-02-18 08:57:00 -0500

    add viewImage screen and stub out buttons

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   
   return (
-   <WelcomeScreen />
+  //  <WelcomeScreen />
+  <ViewImageScreen />
+
   );
 }
