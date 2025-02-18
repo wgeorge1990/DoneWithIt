758a3e7bf0a85b8b76f272b0add56bb27e094830
WILLIAM GEORGE <williameverettgeorge@icloud.com>
2025-02-18 12:25:39 -0500

    styling text

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import { View, Text } from 'react-native';
 
 export default function App() {
   
   return (
   //  <WelcomeScreen />
-  <ViewImageScreen />
+  // <ViewImageScreen />
+
+  <View 
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }} 
+  >
+    <Text style={{
+      // fontFamily: "Courier",
+      fontSize: 30,
+      fontStyle: "italic",
+      fontWeight: 'bold',
+      color: 'tomato',
+      textTransform: "capitalize",
+      textDecorationLine: "underline",
+      textAlign: 'center',
+      lineHeight: 30
+
+    }}
+    >I LOVE REACT NATIVE and it is a lot of fun to work with and here is some more text</Text>
+    
+  </View>
 
   );
 }
