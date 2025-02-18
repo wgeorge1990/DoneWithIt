faa9248a81e32814ccc5aa94c7ede9767b211bf6
WILLIAM GEORGE <williameverettgeorge@icloud.com>
2025-02-18 12:36:08 -0500

    encapsulating text styles

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import { View, Text } from 'react-native';
 
+import AppText from './app/components/AppText';
+
 export default function App() {
   
   return (
@@ -14,22 +16,11 @@ export default function App() {
       flex: 1,
       justifyContent: "center",
       alignItems: "center"
-    }} 
-  >
-    <Text style={{
-      // fontFamily: "Courier",
-      fontSize: 30,
-      fontStyle: "italic",
-      fontWeight: 'bold',
-      color: 'tomato',
-      textTransform: "capitalize",
-      textDecorationLine: "underline",
-      textAlign: 'center',
-      lineHeight: 30
-
     }}
-    >I LOVE REACT NATIVE and it is a lot of fun to work with and here is some more text</Text>
-    
+  >
+    <AppText>
+      I LOVE REACT NATIVE
+    </AppText>
   </View>
 
   );
